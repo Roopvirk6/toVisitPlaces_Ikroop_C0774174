b62d2ee35e8703c6714b6d266a8a8fd260a11d4f
Roopvirk6 <virkikroop13@gmail.com>
2020-06-14 23:49:52 -0400

    Initial Commit

---
+++ toVisitPlaces_Ikroop_C0774174Tests/toVisitPlaces_Ikroop_C0774174Tests.swift
@@ -0,0 +1,34 @@
+//
+//  toVisitPlaces_Ikroop_C0774174Tests.swift
+//  toVisitPlaces_Ikroop_C0774174Tests
+//
+//  Created by VirkIkroop on 2020-06-14.
+//  Copyright © 2020 VirkIkroop. All rights reserved.
+//
+
+import XCTest
+@testable import toVisitPlaces_Ikroop_C0774174
+
+class toVisitPlaces_Ikroop_C0774174Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
